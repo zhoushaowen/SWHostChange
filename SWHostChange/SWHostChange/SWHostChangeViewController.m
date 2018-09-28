d1ab3c0b434f7116a60b6135ce334a55f97835c0
zhoushaowen <348345883@qq.com>
2018-09-28 09:15:24 +0800

    适配ios12

--- SWHostChange/SWHostChange/SWHostChangeViewController.m
+++ SWHostChange/SWHostChange/SWHostChangeViewController.m
@@ -108,13 +108,13 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self dismissViewControllerAnimated:YES completion:nil];
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"是否修改当前Host?" message:@"修改当前Host之后app会自动退出,请重新启动app!" preferredStyle:UIAlertControllerStyleAlert];
     [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
         SWHost *host = [SWHostChangeManager sharedInstance].hostGroup[indexPath.row];
         [[SWHostChangeManager sharedInstance] setCurrentHost:host];
     }]];
     [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil]];
-//    [self presentViewController:alert animated:YES completion:nil];
     [[[UIApplication sharedApplication].delegate window].rootViewController presentViewController:alert animated:YES completion:nil];
 }
 
